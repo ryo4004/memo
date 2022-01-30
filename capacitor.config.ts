08e4448c5aead4d720c4a0fdb5d8804315af287f
ryo4004 <kato@ryo4004.net>
2022-01-30 18:55:06 +0900

    capacitor.config.tsを更新

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.zatsuzen.memo',
   appName: 'memo',
   webDir: 'build',
   bundledWebRuntime: false
