2786bdb1e417f2bb1ed38b4ee86ccb552e4a8b17
ryo4004 <kato@ryo4004.net>
2022-01-30 15:02:46 +0000

    詳細ページを更新

--- src/pages/TodoList/Detail/Detail.tsx
+++ src/pages/TodoList/Detail/Detail.tsx
@@ -10,26 +10,44 @@ import {
   IonPage,
   IonToolbar,
   // useIonViewWillEnter,
+  IonNote,
 } from '@ionic/react'
 import { useParams } from 'react-router'
 
+import { useTodoContext } from '../../../hooks/useTodo'
+
+import styles from './Detail.module.scss'
+
 export const Detail = () => {
   const params = useParams<{ id: string }>()
+  const id = Number(params.id)
 
-  const id = params.id
+  const { todoList } = useTodoContext()
+  const todo = todoList.find((todo) => todo.id === id)
 
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonButtons slot="start">
-            <IonBackButton text="Inbox" defaultHref="/"></IonBackButton>
+            <IonBackButton text="戻る" defaultHref="/"></IonBackButton>
           </IonButtons>
         </IonToolbar>
       </IonHeader>
 
-      <IonContent fullscreen>
-        <div>{id ? id : 'not found'}</div>
+      <IonContent>
+        {!todo && (
+          <div className={styles.note}>
+            <IonNote>みつかりませんでした</IonNote>
+          </div>
+        )}
+        {todo && (
+          <>
+            <div>{todo.label}</div>
+            <div>{todo.span}</div>
+            <div>{todo.lastDate ? todo.lastDate.toFormat('yyyy/M/d') : 'なし'}</div>
+          </>
+        )}
       </IonContent>
     </IonPage>
   )
