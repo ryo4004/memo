f6bce403f69d4d9a08f986931122a00becd47765
ryo4004 <kato@ryo4004.net>
2022-02-03 14:37:45 +0000

    styleを調整

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,6 +1,8 @@
-import { IonLabel, IonItem, IonCard, IonCardContent, IonNote } from '@ionic/react'
+import { IonLabel, IonItem, IonCard, IonBadge } from '@ionic/react'
 
 import type { Todo } from '../../hooks/useTodo'
+
+import styles from './TodoItem.module.scss'
 
 type Props = {
   todo: Todo
@@ -10,18 +12,24 @@ export const TodoItem = (props: Props) => {
   const { todo } = props
   return (
     <IonCard key={todo.id} routerLink={'/detail/' + todo.id}>
-      <IonItem>
+      <IonItem lines="full">
         <IonLabel>{todo.label}</IonLabel>
       </IonItem>
 
-      <IonCardContent>
-        <div>
-          <IonNote>{todo.span}</IonNote>
-        </div>
-        <div>
-          <IonNote>{todo.lastDate && todo.lastDate.toFormat('yyyy/M/d')}</IonNote>
-        </div>
-      </IonCardContent>
+      <div className={styles.content}>
+        <IonItem lines="none">
+          <IonBadge className={styles.badge} color="medium">
+            実施間隔
+          </IonBadge>
+          <IonLabel className={styles.label}>{todo.span}日ごとに実施</IonLabel>
+        </IonItem>
+        <IonItem lines="none">
+          <IonBadge className={styles.badge} color="medium">
+            最終実施日
+          </IonBadge>
+          <IonLabel className={styles.label}>{todo.lastDate && todo.lastDate.toFormat('yyyy/M/d')}</IonLabel>
+        </IonItem>
+      </div>
     </IonCard>
   )
 }
