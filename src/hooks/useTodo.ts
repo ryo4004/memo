da07f46145e240d139711a307d88bbfd39cb4586
ryo4004 <kato@ryo4004.net>
2022-02-11 09:07:04 +0000

    やることの入力処理を切り出す

--- src/hooks/useTodo.ts
+++ src/hooks/useTodo.ts
@@ -9,30 +9,26 @@ const STORAGE_KEY = 'todolist'
 type State = {
   roomList: Array<Room>
   todoList: Array<Todo>
-  todoInput: TodoInput
 }
 
 type TodoType = State & {
   addRoom: () => void
   removeRoom: () => void
-  addTodo: () => boolean
+  addTodo: (validatedInput: Todo) => void
   removeTodo: (id: number) => void
-  updateInput: (type: InputType, value: string | null | undefined) => void
 }
 
 const initState: State = {
   roomList: [],
   todoList: [],
-  todoInput: { label: '', span: '', lastDate: '' },
 }
 
 export const TodoContext = createContext<TodoType>({
   ...initState,
   addRoom: noop,
   removeRoom: noop,
-  addTodo: () => false,
+  addTodo: noop,
   removeTodo: noop,
-  updateInput: noop,
 })
 
 export const useTodoContext = () => {
@@ -51,20 +47,8 @@ export type Todo = {
   lastDate: DateTime | null
 }
 
-type TodoInput = {
-  label: string
-  span: string
-  lastDate: string
-}
-
-type InputType = keyof TodoInput
-
-const initInput: TodoInput = { label: '', span: '', lastDate: '' }
-
 export const useTodo = (): TodoType => {
-  const [roomList, setRoomList] = useState<Array<Room>>([])
-  const [todoList, setTodoList] = useState<Array<Todo>>([])
-  const [todoInput, setTodoInput] = useState<TodoInput>(initInput)
+  const [state, setState] = useState<State>(initState)
 
   useEffect(() => {
     // eslint-disable-next-line
@@ -76,56 +60,42 @@ export const useTodo = (): TodoType => {
           ...todo,
           lastDate: todo.lastDate ? DateTime.fromMillis(Number(todo.lastDate)) : null,
         }))
-        setTodoList(newList)
+        setState((state) => ({ ...state, todoList: newList }))
       }
     })()
   }, [])
 
-  const updateInput = (type: InputType, value: string | null | undefined) => {
-    if (value) {
-      const newInput = {
-        ...todoInput,
-        [type]: value,
-      }
-      setTodoInput(newInput)
-    }
-  }
-
   const addRoom = () => {
-    setRoomList([])
+    setState((state) => ({ ...state, roomList: [] }))
   }
 
   const removeRoom = () => {
-    setRoomList([])
+    setState((state) => ({ ...state, roomList: [] }))
   }
 
-  const addTodo = () => {
-    if (todoInput.label !== '') {
-      const newTodo = {
-        id: new Date().getTime(),
-        label: todoInput.label,
-        span: Number(todoInput.span),
-        lastDate: DateTime.fromFormat(todoInput.lastDate, 'yyyy-M-d'),
-      }
-      const newList = [...todoList, newTodo]
-      setTodoList(newList)
-      storage.setData(STORAGE_KEY, {
-        list: newList.map((l) => ({ ...l, lastDate: l.lastDate ? l.lastDate.toMillis() : null })),
-      })
-      setTodoInput(initInput)
-      return true
-    } else {
-      return false
-    }
+  const addTodo = (validatedInput: Todo) => {
+    const newList = [...state.todoList, validatedInput]
+    storage.setData(STORAGE_KEY, {
+      list: newList.map((l) => ({ ...l, lastDate: l.lastDate ? l.lastDate.toMillis() : null })),
+    })
+    setState((state) => ({ ...state, todoList: newList }))
+    return true
   }
 
   const removeTodo = (id: number) => {
-    const newTodoList = todoList.filter((todo) => todo.id !== id)
-    setTodoList(newTodoList)
+    const newTodoList = state.todoList.filter((todo) => todo.id !== id)
+    setState((state) => ({ ...state, todoList: newTodoList }))
     storage.setData(STORAGE_KEY, {
       list: newTodoList.map((l) => ({ ...l, lastDate: l.lastDate ? l.lastDate.toMillis() : null })),
     })
   }
 
-  return { roomList, addRoom, removeRoom, todoList, addTodo, removeTodo, todoInput, updateInput }
+  return {
+    roomList: state.roomList,
+    addRoom,
+    removeRoom,
+    todoList: state.todoList,
+    addTodo,
+    removeTodo,
+  }
 }
