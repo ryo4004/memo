b1e917a4a207614c9e9ffadfd5ade099978759f9
ryo4004 <kato@ryo4004.net>
2022-02-11 15:14:01 +0000

    storageの関数を追加

--- src/utilities/storage.ts
+++ src/utilities/storage.ts
@@ -1,5 +1,7 @@
 import { Storage } from '@capacitor/storage'
+import { DateTime } from 'luxon'
 
+// 基本
 export const setData = async (key: string, value: object) => {
   return await Storage.set({ key, value: JSON.stringify(value) })
 }
@@ -12,3 +14,34 @@ export const getData = async (key: string) => {
 export const removeData = async (key: string) => {
   return await Storage.remove({ key })
 }
+
+// 配列
+const timeConvertToDateTime = <T>(array: Array<T & { lastDate: string }>): Array<T & { lastDate: DateTime | null }> => {
+  return array.map((value) => ({
+    ...value,
+    lastDate: value.lastDate ? DateTime.fromMillis(Number(value.lastDate)) : null,
+  }))
+}
+
+const timeConvertToMillis = <T>(array: Array<T & { lastDate?: DateTime }>) => {
+  return array.map((value) => ({
+    ...value,
+    lastDate: value.lastDate ? value.lastDate.toMillis() : null,
+  }))
+}
+
+export const loadArray = async <T>(key: string): Promise<Array<T>> => {
+  const storageData = await getData(key)
+  if (!storageData) {
+    return []
+  }
+  const parsedData: { list: Array<any> } = JSON.parse(storageData)
+  const list = timeConvertToDateTime(parsedData.list)
+  return list
+}
+
+export const saveArray = async <T>(key: string, array: Array<T>): Promise<void> => {
+  await setData(key, timeConvertToMillis(array))
+}
+
+export const removeArray = async (key: string) => await removeData(key)
